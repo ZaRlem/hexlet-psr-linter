2fdffafe69ed5a510008fa604e1daedc50caacd3
Zakhar <shelz@ukr.net>
2016-08-12 20:11:14 +0300

    fix syntax errors

--- src/filesystem.php
+++ src/filesystem.php
@@ -4,26 +4,26 @@ namespace HexletPsrLinter;
 
 function getFilePathList($path)
 {
-$funcIter = function($path, $files) use (&$funcIter){
+    $funcIter = function ($path, $files) use (&$funcIter) {
 
-    if(is_dir($path)){
-        $handle = opendir($path);
-        while (false !== ($file = readdir($handle))){
-            if($file === '.' || $file === '..'){
-                continue;
-            } elseif(is_file($path . '/' . $file) &&
-             (pathInfo($path . '/' . $file)['extension'] == 'php')) {
-                $files[] = $path . '/' . $file;
+        if (is_dir($path)) {
+            $handle = opendir($path);
+            while (false !== ($file = readdir($handle))) {
+                if ($file === '.' || $file === '..') {
+                    continue;
+                } elseif (is_file($path . '/' . $file) &&
+                 (pathInfo($path . '/' . $file)['extension'] == 'php')) {
+                    $files[] = $path . '/' . $file;
+                }
+                if (is_dir($path . '/' . $file)) {
+                    $files = array_merge($files, $funcIter($path . '/' . $file, []));
+                }
             }
-            if(is_dir($path . '/' . $file)){
-              $files = array_merge($files, $funcIter($path . '/' . $file, []));
-            }
+            closedir($handle);
+        } elseif (pathInfo($path) == 'php') {
+            $files[] = $path;
         }
-        closedir($handle);
-    } elseif(pathInfo($path) == 'php') {
-      $files[] = $path;
-    }
-   return $files;
-};
- return $funcIter($path, []);
+           return $files;
+    };
+    return $funcIter($path, []);
 }
