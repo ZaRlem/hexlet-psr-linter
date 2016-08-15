5b1108058e799b0a53fa3db91a53a6f28e914144
Zakhar <shelz@ukr.net>
2016-08-15 23:20:12 +0300

    add tests

--- tests/LinterTest.php
+++ tests/LinterTest.php
@@ -6,30 +6,75 @@ use function HexletPsrLinter\lint;
 
 class LinterTest extends \PHPUnit_Framework_TestCase
 {
-    public function testLinter()
+    public function testFuncChecker()
     {
-        $wrongCode = '
-          <?php
-          function get_Amount()
-          {
-            return amount;
-          }
-          function negate()
-          {
-          return new Money(-1 * amount);
-          };';
         $rightCode = '
-          <?php
-          function getAmount()
-          {
-            return amount;
-          }
-          function negate()
-          {
-          return new Money(-1 * amount);
-          };';
-
-        $this->assertTrue(!empty(lint($wrongCode)));
-        $this->assertTrue(empty(lint($rightCode)));
+            <?php
+            function getAmount()
+            {
+                return $amount;
+            }';
+        $wrongCode = '
+            <?php
+            function get_Amount()
+            {
+                return $amount;
+            }';
+        $this->assertNull(lint($rightCode));
+        $this->assertContains([3,
+            'Warning',
+            'Function names must be declared in camelCase.'],
+            lint($wrongCode));
+    }
+    public function testVarChecker()
+    {
+        $rightCode = '
+            <?php
+            function getAmount()
+            {
+                return $amount;
+            }';
+        $wrongCode = '
+            <?php
+            function getAmount()
+            {
+                return $am_ount;
+            }';
+        $this->assertNull(lint($rightCode));
+        $this->assertContains([5,
+            'Warning',
+            'Variable names must be declared in camelCase.'],
+            lint($wrongCode));
+    }
+    public function testSideEffectsChecker()
+    {
+        $rightCode = '
+        <?php
+        function foo()
+        {
+            // function body
+        }
+        if (! function_exists("bar")) {
+            function bar()
+            {
+                // function body
+            }
+        }
+        ';
+        $wrongCode = '
+        <?php
+        ini_set("error_reporting", E_ALL);
+        include "file.php";
+        echo "<html>\n";
+        function foo()
+            {
+                // function body
+            }
+        ';
+        $this->assertNull(lint($rightCode));
+        $this->assertContains([0,
+            'Warning',
+            'Declaration of symbols and side effects in one file'],
+            lint($wrongCode));
     }
 }
